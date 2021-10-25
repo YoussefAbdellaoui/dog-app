c0cd36d8de85f392cf88927438c53e4fa3cd1453
Michael Ignat <michael.ignat13@gmail.com>
2021-10-25 13:32:26 +0800

    Fixed next/previous dog tracking, Added error state in UI & max 10 previous dogs stored

--- client/src/components/Dog.tsx
+++ client/src/components/Dog.tsx
@@ -3,10 +3,19 @@ import FavouriteDog from "./FavouriteDog";
 import Picture from "./ui/Picture";
 
 const Dog = () => {
+  // Generic dog state used to display the current dog
   const [newDog, setNewDog] = useState(Object);
+
+  // Loading state
   const [loading, setLoading] = useState<boolean>(true);
+
+  // Array states for keeping track of dogs & favourite dogs
   const [dogArray, setDogArray] = useState<string[]>([]);
   const [favDogArray, setFavDogArray] = useState<string[]>([]);
+
+  // Error states for showing in the UI
+  const [prevDogError, setPrevDogError] = useState(false);
+  const [favDogError, setFavDogError] = useState(false);
 
   // Query the API for a dog
   const getDog = async () => {
@@ -21,33 +30,86 @@ const Dog = () => {
 
   // Fetch a new dog from the API
   const fetchDog = async () => {
+    // Show loading state on screen
     setLoading(true);
+
     const dog = await getDog();
     setNewDog(dog.message);
+
+    // Check if there are 10 dogs stored in the array & remove the first index
+    if (dogArray.length > 10) {
+      // Remove the first index from the array
+      setDogArray(dogArray.splice(0, 1));
+
+      // Add the current dog to the array
+      setDogArray([...dogArray, dog.message]);
+      setLoading(false);
+      return;
+    }
+
+    // Otherwise just add the current dog to the array
     setDogArray([...dogArray, dog.message]);
     setLoading(false);
   };
 
-  // Favourite dog
   const favDog = () => {
     // Check if the favourite dog already exists
     const checkFav = favDogArray.find((element) => newDog === element);
+
+    // If we find the current dog in the array already
     if (checkFav) {
-      return console.log("Favourite already exists");
+      setFavDogError(true);
+      return;
     }
+
+    // Add the dog to the favourite dog array
     setFavDogArray([...favDogArray, newDog]);
   };
 
-  // Previous dog
-  const prevDog = () => {
-    // -2 as the last index of the array is the current dog shown on screen
-    const lastIndex = dogArray.length - 2;
+  const removeFav = () => {
+    console.log("testing");
+  };
 
-    if (!dogArray[lastIndex]) {
-      return console.log("No previous dog");
+  const prevDog = () => {
+    // Get the index of the previous dog
+    const previousDogIndex = dogArray.indexOf(newDog) - 1;
+
+    // If we're below the index of 0 then show that there is no previous dog
+    if (previousDogIndex < 0) {
+      setPrevDogError(true);
+      return;
     }
 
-    setNewDog(dogArray[lastIndex]);
+    // Remove the the favDogError from the UI if it exists
+    if (favDogError) {
+      setFavDogError(false);
+    }
+
+    // Otherwise set the previous dog to the current dog so we can show it on screen
+    setNewDog(dogArray[previousDogIndex]);
+  };
+
+  const nextDog = () => {
+    // If we're showing the previous dog error then remove it
+    if (prevDogError) {
+      setPrevDogError(false);
+    }
+
+    // Remove the the favDogError from the UI if it exists
+    if (favDogError) {
+      setFavDogError(false);
+    }
+
+    // Get the index of the next dog
+    const nextDogIndex = dogArray.indexOf(newDog) + 1;
+
+    // If the next index exists we've already fetched a dog, therefore set it to the current dog
+    if (nextDogIndex in dogArray) {
+      return setNewDog(dogArray[nextDogIndex]);
+    }
+
+    // Otherwise just fetch a new dog
+    fetchDog();
   };
 
   // Load the dog on initial page load
@@ -67,6 +129,11 @@ const Dog = () => {
             <button className="btn btn--fav" onClick={favDog}>
               Favourite
             </button>
+            {favDogError && (
+              <span className="error fav__error">
+                Favourite already exists!
+              </span>
+            )}
           </div>
 
           <div className="dog__main">
@@ -76,6 +143,9 @@ const Dog = () => {
                   Previous
                 </button>
               </div>
+              {prevDogError && (
+                <span className="error previous__error">No Previous Dog!</span>
+              )}
             </div>
 
             <div className="picture">
@@ -84,7 +154,7 @@ const Dog = () => {
 
             <div className="next">
               <div className="next--wrapper">
-                <button className="btn btn--next" onClick={fetchDog}>
+                <button className="btn btn--next" onClick={nextDog}>
                   Next
                 </button>
               </div>
@@ -93,7 +163,7 @@ const Dog = () => {
         </div>
       </section>
       <div className="separator"></div>
-      <FavouriteDog favouriteDogs={favDogArray} />
+      <FavouriteDog favouriteDogs={favDogArray} removeFav={() => removeFav} />
     </>
   );
 };
--- client/src/components/FavouriteDog.tsx
+++ client/src/components/FavouriteDog.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 interface Props {
   favouriteDogs: Array<string>;
+  removeFav: (event: React.MouseEvent<HTMLImageElement>) => void;
 }
 
-const FavouriteDog: React.FC<Props> = ({ favouriteDogs }) => {
+const FavouriteDog: React.FC<Props> = ({ favouriteDogs, removeFav }) => {
   return (
     <section id="favourite">
       <h1 className="header__title favourite__title">
@@ -12,9 +13,15 @@ const FavouriteDog: React.FC<Props> = ({ favouriteDogs }) => {
       </h1>
 
       <div className="dog__fav">
+        {/* better to use forEach since we're not using map's value? */}
         {favouriteDogs.map((dog) => (
           <figure key={dog} className="dog__fav--wrapper">
-            <img src={dog} alt="" className="dog__img dog__fav--img" />
+            <img
+              src={dog}
+              alt=""
+              className="dog__img dog__fav--img"
+              onClick={() => removeFav}
+            />
           </figure>
         ))}
       </div>
